99eeea893076ef38f8cd8bdd07c0981f1803bf48
HarunTeper <hteper@web.de>
2024-04-03 13:45:51 +0000

    added first functions for single threaded

--- packages/src/anytime_examples/include/anytime_examples/anytime_node.hpp
+++ packages/src/anytime_examples/include/anytime_examples/anytime_node.hpp
@@ -4,8 +4,13 @@ class AnytimeST : public rclcpp::Node
 {
     public:
         AnytimeST();
+        ~AnytimeST();
 
     private:
+
+    // create a std::thread
+    void resource_thread();
+    std::thread resource_thread_;
 };
 
 class AnytimeMT : public rclcpp::Node
--- packages/src/anytime_examples/src/anytime_node.cpp
+++ packages/src/anytime_examples/src/anytime_node.cpp
@@ -3,6 +3,26 @@
 AnytimeST::AnytimeST() : Node("anytime")
 {
     std::cout << "Single threaded Node Started" << std::endl;
+
+    // Start the resource thread
+    resource_thread_ = std::thread(&AnytimeST::resource_thread, this);
+}
+
+AnytimeST::~AnytimeST()
+{
+    // Join the resource thread
+    resource_thread_.join();
+}
+
+void AnytimeST::resource_thread()
+{
+    std::cout << "Resource Thread Started" << std::endl;
+    rclcpp::Rate rate(1);
+    while (rclcpp::ok())
+    {
+        std::cout << "Resource Thread Running" << std::endl;
+        rate.sleep();
+    }
 }
 
 AnytimeMT::AnytimeMT() : Node("anytime")
