481cce197aeacadf6ee771809aac93e266648442
HarunTeper <harun.teper@tu-dortmund.de>
2025-03-25 15:38:19 +0000

    Remove auto_install script and tasks.json configuration files

--- auto_install.sh
+++
@@ -1,4 +0,0 @@
-cd src
-wget https://download.pytorch.org/libtorch/cu117/libtorch-shared-with-deps-2.0.1%2Bcu117.zip
-unzip libtorch-shared-with-deps-2.0.1+cu117.zip
-rm libtorch-shared-with-deps-2.0.1+cu117.zip
